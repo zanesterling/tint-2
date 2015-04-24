12ec45ce7ae52b3c037597abd959951d0eaee2a5
Zane Sterling <sterling.zane@gmail.com>
2015-04-24 12:15:01 -0400

    Remove useless (?) backend

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+import db
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return 'hello home'
+	return render_template('home.html')
 
 if __name__=='__main__':
 	app.run('0.0.0.0', 9000, debug=True)
