96a53ca9e3c61ff07cc6fb1c1c86f736353223ee
Zane Sterling <sterling.zane@gmail.com>
2015-04-27 08:39:31 -0400

    Hook up the login process to the database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, \
+	session
 from flask.ext.github import GitHub
 
 import db
@@ -17,13 +18,37 @@ def home():
 
 @app.route('/login')
 def login():
+	# if the user is already logged in, send them away
+	if 'username' in session:
+		return redirect(url_for('home'))
+
+	# forward to github's oauth system
 	return github.authorize()
 
 @app.route('/github-callback')
 @github.authorized_handler
 def gh_callback(oauth_token):
+	# ensure we got the token
+	if oauth_token is None:
+		flash('There was an issue logging in')
+		log('err: login error')
+		return redirect(url_for('home'))
+
+	# if the user is new register them with the db
+	usern = db.hasUser(oauth_token)
+	if not usern:
+		usern = db.addUser(oauth_token)
+
+	# give them a cookie (or refresh theirs) that says logged-in
+	session['username'] = usern
+
+	# send them to the dashboard
 	flash('You were logged in')
 	return redirect(url_for('home'))
+
+# TODO add proper error logging
+def log(logstr):
+	print logstr
 
 if __name__=='__main__':
 	app.run('0.0.0.0', 9000, debug=True)
