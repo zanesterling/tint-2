2571f2bda6d1fb1d735e40295f7ffb0d4e7383b2
Zane Sterling <sterling.zane@gmail.com>
2015-04-26 21:23:57 -0400

    Add simple github auth on /login

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+from flask.ext.github import GitHub
 
 import db
+import secrets
 
 app = Flask(__name__)
+app.config['GITHUB_CLIENT_ID'] = secrets.CLIENT_ID
+app.config['GITHUB_CLIENT_SECRET'] = secrets.CLIENT_SECRET
+
+github = GitHub(app)
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+
+@app.route('/login')
+def login():
+	return github.authorize()
+
+@app.route('/github-callback')
+def gh_callback():
+	return "called back bangarang"
 
 if __name__=='__main__':
 	app.run('0.0.0.0', 9000, debug=True)
